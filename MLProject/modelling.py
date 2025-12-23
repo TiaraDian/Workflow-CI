156a759b618a993921bba885839e89f926892b6d
Tiara Diansyah Putri <tiaradiansyah113@gmail.com>
2025-12-23 18:57:49 +0700

    initial commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import mlflow
+import mlflow.sklearn
+
+# Load dataset preprocessing
+DATA_PATH = r"D:\SMSML_Tiara Diansyah Putri\Workflow-CI\MLProject\Heart_Disease_Prediction_preprocessing.csv"
+df = pd.read_csv(DATA_PATH)
+
+# Pisahkan fitur & target
+X = df.drop(columns=["Heart Disease"])
+y = df["Heart Disease"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Aktifkan MLflow autolog
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    print("Accuracy:", acc)
