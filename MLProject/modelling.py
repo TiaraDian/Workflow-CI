a5b89d057ca6064c3965f6613bd7bfcd96fb7f17
Tiara Diansyah Putri <tiaradiansyah113@gmail.com>
2025-12-24 00:50:58 +0700

    coba lagi

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score
 import mlflow
 import mlflow.sklearn
 import os
@@ -11,20 +11,28 @@ DATA_PATH = os.path.join(BASE_DIR, "Heart_Disease_Prediction_preprocessing.csv")
 
 df = pd.read_csv(DATA_PATH)
 
-# Pisahkan fitur & target
 X = df.drop(columns=["Heart Disease"])
 y = df["Heart Disease"]
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
 mlflow.sklearn.autolog()
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+with mlflow.start_run():
 
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-print("Accuracy:", acc)
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+
+    print("Accuracy:", acc)
