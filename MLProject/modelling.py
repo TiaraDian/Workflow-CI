1164d6a9b4462bb2f3b59e7e62d6a2957c760683
Tiara Diansyah Putri <tiaradiansyah113@gmail.com>
2025-12-24 08:14:58 +0700

    without docker dah

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,24 +1,14 @@
-import os
 import pandas as pd
-import mlflow
-import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
 
-while mlflow.active_run() is not None:
-    mlflow.end_run()
+DATA_PATH = "Heart_Disease_Prediction_preprocessing.csv"
+df = pd.read_csv(DATA_PATH)
 
-os.environ.pop("MLFLOW_RUN_ID", None)
-os.environ.pop("MLFLOW_EXPERIMENT_ID", None)
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-mlflow.set_tracking_uri(f"sqlite:///{os.path.join(BASE_DIR, 'mlflow.db')}")
-mlflow.set_experiment("heart-disease-ci")
-
-df = pd.read_csv(os.path.join(BASE_DIR, "Heart_Disease_Prediction_preprocessing.csv"))
-X = df.drop(columns=["Heart Disease"]).astype("float64")
+X = df.drop(columns=["Heart Disease"])
 y = df["Heart Disease"]
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -27,15 +17,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="logreg-heart-disease"):
-
+with mlflow.start_run():
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
-
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, artifact_path="model")
 
     print("Accuracy:", acc)
